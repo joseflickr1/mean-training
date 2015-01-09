b6f2376c7b4859a4d249bdfbfd8e08ea8281bee7
jose <EMAIL>
2015-01-09 20:42:38 +0100

    add forgotton line of code

--- public/app/account/mvProfileCtrl.js
+++ public/app/account/mvProfileCtrl.js
@@ -10,6 +10,10 @@ angular.module('app').controller('mvProfileCtrl', function($scope, mvAuth, mvIde
 			lastName: $scope.lname
 		}
 
+		if($scope.password && $scope.password.length > 0) {
+			newUserData.password = $scope.password;
+		}
+
 		mvAuth.updateCurrentUser(newUserData).then(function() {
 			mvNotifier.notify('Your user account has been updated');
 		}, function(reason) {
