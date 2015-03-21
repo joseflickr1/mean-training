ae0b2439ed41e4256f52dd77c441cdfb80517811
jose <EMAIL>
2015-03-21 13:59:28 +0100

    more for courselist

---
+++ server/models/Course.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var courseSchema = mongoose.Schema({
+	title: {type:String, required: '{PATH} is required!'},
+	featured: {type: Boolean, required: '{PATH} is required!'},
+	published: {type:Date, required: '{PATH} is required!' },
+	tags: [String] 
+});
+var Course = mongoose.model('Course', courseSchema);
+
+function createDefaultCourses() {
+	Course.find({}).exec(function(err, collection) {
+		if(collection.length === 0) {
+			Course.create({title: 'C# for sociopaths', featured: true, published: new Date('10/5/2013'), tags: ['C#']});
+			Course.create({title: 'C# for sociopaths', featured: true, published: new Date('10/5/2013'), tags: ['C#']});
+		}
+	})
+}
+
+exports.createDefaultCourses = createDefaultCourses;
