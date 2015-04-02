2875c45a939cf4c13e91b5740a313ce5ee080b8d
jose <EMAIL>
2015-04-02 18:22:42 +0200

    Course filtering and sorting

--- server/models/Course.js
+++ server/models/Course.js
@@ -12,7 +12,10 @@ function createDefaultCourses() {
 	Course.find({}).exec(function(err, collection) {
 		if(collection.length === 0) {
 			Course.create({title: 'C# for sociopaths', featured: true, published: new Date('10/5/2013'), tags: ['C#']});
-			Course.create({title: 'C# for sociopaths', featured: true, published: new Date('10/5/2013'), tags: ['C#']});
+			Course.create({title: 'A C# for sociopaths', featured: true, published: new Date('09/5/2013'), tags: ['C#']});
+			Course.create({title: 'B C# for sociopaths', featured: true, published: new Date('08/5/2013'), tags: ['C#']});
+			Course.create({title: 'D C# for sociopaths', featured: true, published: new Date('08/5/2015'), tags: ['C#']});
+			Course.create({title: 'E C# for sociopaths', featured: true, published: new Date('08/4/2015'), tags: ['C#']});
 		}
 	})
 }
