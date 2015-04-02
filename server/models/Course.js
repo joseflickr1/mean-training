be861b44c9c3396f9d04f400a13da4781e428c5a
jose <EMAIL>
2015-04-02 18:25:17 +0200

    init data fixes rollback

--- server/models/Course.js
+++ server/models/Course.js
@@ -10,7 +10,7 @@ var Course = mongoose.model('Course', courseSchema);
 
 function createDefaultCourses() {
 	Course.find({}).exec(function(err, collection) {
-		if(collection.length === 2) {
+		if(collection.length === 0) {
 			Course.create({title: 'C# for sociopaths', featured: true, published: new Date('10/5/2013'), tags: ['C#']});
 			Course.create({title: 'A C# for sociopaths', featured: true, published: new Date('09/5/2013'), tags: ['C#']});
 			Course.create({title: 'B C# for sociopaths', featured: true, published: new Date('08/5/2013'), tags: ['C#']});
