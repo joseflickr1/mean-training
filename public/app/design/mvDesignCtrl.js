83239b9e9a36aad942f156a0bac31e6babe9bf3b
jose <EMAIL>
2015-06-14 13:55:51 +0200

    -

--- public/app/design/mvDesignCtrl.js
+++ public/app/design/mvDesignCtrl.js
@@ -6,14 +6,14 @@ angular.module('app').controller('mvDesignCtrl', function($scope) {
     angular.element(document).ready(function(){
 	  angular.element(".owl-carousel").owlCarousel({
 	  	center: false,
-	    items:2,
+	    items:1,
 	    loop:true,
 	    margin:10,
 	    dots: false,
 	    autoWidth: true,
 	    responsive:{
 	        600:{
-	            items:2
+	            items:1
 	        }
 	    }
 	  });
