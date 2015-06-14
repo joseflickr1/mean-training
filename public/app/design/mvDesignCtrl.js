f4c616d78632dc88c1357901fa63dceb36a044de
jose <EMAIL>
2015-06-14 12:00:13 +0200

    -

--- public/app/design/mvDesignCtrl.js
+++ public/app/design/mvDesignCtrl.js
@@ -3,5 +3,23 @@ angular.module('app').controller('mvDesignCtrl', function($scope) {
 
 	$scope.src = "/media/Seoul-P1-4.mp4";
 
+    angular.element(document).ready(function(){
+	  angular.element(".owl-carousel").owlCarousel({
+	  	center: false,
+	    items:2,
+	    loop:true,
+	    margin:10,
+	    dots: false,
+	    smartSpeed: 1000,
+	    autoWidth: true,
+	    responsive:{
+	        600:{
+	            items:4
+	        }
+	    }
+	  });
+	});
+
+
 
 });
